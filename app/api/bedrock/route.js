4bc2967ac4fc7cf77b4fb7f7ed298c00c9c464e2
Alicia Fernandez <fernandez.m.alicia2020@gmail.com>
2025-01-06 02:12:46 -0500

    updated env vars for aws, conflicting names with netfliy

--- app/api/bedrock/route.js
+++ app/api/bedrock/route.js
@@ -43,8 +43,8 @@ export async function POST(req) {
     const client = new BedrockAgentRuntimeClient({
       region: "us-east-2",
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
       },
     });
 
